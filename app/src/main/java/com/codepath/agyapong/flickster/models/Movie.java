d3dda96b4e4b5cd7659e981c0d4eb4cca4da0e81
agyapongeli77 <agyapongeli77@gmail.com>
2019-03-01 01:01:45 -0500

    1st bonus feature of landscape mode added

--- app/src/main/java/com/codepath/agyapong/flickster/models/Movie.java
+++ app/src/main/java/com/codepath/agyapong/flickster/models/Movie.java
@@ -11,9 +11,14 @@ public class Movie {
     String posterPath;
     String title;
     String overview;
+    String backdropPath;
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w300/%s", backdropPath);
     }
 
     public String getTitle() {
@@ -24,10 +29,13 @@ public class Movie {
         return overview;
     }
 
+
     public Movie (JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        backdropPath = jsonObject.getString("backdrop_path");
+
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
